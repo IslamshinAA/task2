6875d3d726d1ef5694217f57c23cb8aaaf5d291b
AntonIsl <arshavin0511@gmail.com>
2023-05-11 17:40:02 +0300

    ready

---
+++ task3.java
@@ -0,0 +1,28 @@
+public class task3 {
+    // Дан следующий код, исправьте его там, где требуется (задание 3
+
+
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234); // тут можно передать null
+            int[] abc = {1, 2};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) { // перенес с первого места в конец. т.к. все исключения будут попадать под него
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws NullPointerException {
+        // заменила  FileNotFoundException на NullPointerException- т.к. в методе нет чтения файла а null возможно передать
+        System.out.println(a + b);
+    }
+
+}
+ 
